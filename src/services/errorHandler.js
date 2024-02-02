365d539b9e9346141a8c5a8e89558f4f00c8df6f
David Bishop <davidbish2002@hotmail.com>
2024-02-02 14:02:31 -0330

    Update errorHandler.js

--- src/services/errorHandler.js
+++ src/services/errorHandler.js
@@ -4,3 +4,21 @@ export default (func, error) => {
   console.error(`${func} Request ERROR:\n`, error);
   history.push("/error-500");
 };
+
+export const showError = (res, dispatch, customMessage) => {
+  dispatch(
+    addNotification({
+      error: true,
+      content:
+        res.errors && Object.keys(res.errors).length
+          ? res.errors[Object.keys(res.errors)[0]][0]
+          : res.message
+          ? res.message
+          : customMessage
+          ? customMessage
+          : "Unexpected Error",
+      close: false,
+      duration: 0,
+    })
+  );
+};
