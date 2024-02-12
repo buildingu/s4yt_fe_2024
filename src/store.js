a4111100388dd1d86c1886d7a00929020a94601e
David Bishop <davidbish2002@hotmail.com>
2024-02-11 21:43:19 -0330

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,19 +1,8 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import helpers from "@root/helpers";
 import Reducers from "@reducers";
 
 const middleware = [thunk.withExtraArgument(helpers)];
-const persistedReducer = persistReducer(
-  { key: "root", storage, blacklist: ["notifications"] },
-  Reducers
-);
 
-export const store = createStore(
-  persistedReducer,
-  {},
-  applyMiddleware(...middleware)
-);
-export const persistor = persistStore(store);
+export const store = createStore(Reducers, {}, applyMiddleware(...middleware));
